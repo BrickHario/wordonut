f42c7f5cebb247d9edcbc2aaa1cc34d43c67f18d
Harald-Anton Müller <hmueller.mmt-b2023@fh-salzburg.ac.at>
2025-02-06 19:37:12 +0100

    fix js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import mobileMenu from "./mobile-menu.js";
-import toggleInfo from "./toggle-info.js";
+import mobileMenu from "mobile-menu";
+import toggleInfo from "toggle-info";
 
 document.addEventListener("turbo:load", () => {
   mobileMenu();
